58680afceb6ea673d19c1e9ad71b7e21ea6f5f16
rchen2004 <chen.rya@northeastern.edu>
2026-09-11 12:19:52 -0400

    completed 1.3.7

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -4,6 +4,7 @@ import ListTags from "./ListTags";
 import Tables from "./Tables";
 import Images from "./Images";
 import Forms from "./forms/Forms";
+import HighlightedParagraph from "./HighlightedParagraph";
 
 export default function Lab1() {
   return (
@@ -16,6 +17,7 @@ export default function Lab1() {
       <Tables/> 
       <Images/>
       <Forms/>
+      <HighlightedParagraph/>
       {/* do the next exercise here */}
     </div>
   );
